351e7eab96f030bad502837265102b7ddefe81b2
Kim Honrada <kimhonrada21@gmail.com>
2021-11-17 23:32:31 -0500

    Add initial update endpoints for reviews

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
-const { pool, getReviews, getMeta } = require("../database/index.js");
+const {
+  pool,
+  getReviews,
+  getMeta,
+  addReview,
+} = require("../database/index.js");
 
 const app = express();
 app.use(express.json());
@@ -22,11 +27,32 @@ app.listen(port, () => {
 app.get("/reviews", (req, res) => {
   //console.log("this is the req ", req.query);
   getReviews(req.query)
-    .then((result) => {
-      res.status(200).send(result.rows);
+    .then(([result, photos]) => {
+      console.log(photos);
+      let resObj = {
+        product: req.query.product_id,
+        page: req.query.page || 0,
+        count: result.rowCount || 5,
+        result: result.rows,
+      };
+      res.status(200).send(resObj);
     })
     .catch((err) => {
       res.status(404).send(err);
+    });
+});
+
+// post request to add a review
+app.post("/reviews", (req, res) => {
+  // console.log(req.body);
+  addReview(req.body)
+    .then((result) => {
+      console.log(result);
+      res.send("success");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("failed");
     });
 });
 
@@ -40,3 +66,9 @@ app.get("/reviews/meta", (req, res) => {
       res.status(404).send(err);
     });
 });
+
+// update reviews as helpful
+app.put("/reviews/:review_id/helpful", (req, res) => {});
+
+// update reviews as reported
+app.put("/reviews/:review_id/report", (req, res) => {});
